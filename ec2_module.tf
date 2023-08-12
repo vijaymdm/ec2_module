a26d4e70e25fd409a28d2ad3a0bd578734e5d128
vijay <vijaymdm@gmail.com>
2023-08-12 21:28:26 +0530

    initial_commit

---
+++ ec2_module.tf
@@ -0,0 +1,16 @@
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "single-instance"
+
+  instance_type          = "t2.micro"
+  key_name               = "user1"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-0270753a884536229"]
+  subnet_id              = "subnet-0528842e52e2c7e2e"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
